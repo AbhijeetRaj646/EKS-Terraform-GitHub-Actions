5d2ac1cbab99887c61b2bf7027332563d399f56c
Abhijeet Raj <104703498+AbhijeetRaj646@users.noreply.github.com>
2024-12-19 14:16:20 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = ">= 1.9.3, < 2.0.0"
+  required_version = "~> 1.9.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.49.0"
     }
   }
   backend "s3" {
-    bucket         = "abhi-ews-baket1122"
+    bucket         = "ABHI2-ews-baket1122"
     region         = "us-east-2"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
